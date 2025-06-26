9ad552ae21a0fdeb3957c250c4f8a26cdf40a244
joel.trousset-ext <joel.trousset-ext@mousquetaires.com>
2025-06-26 10:17:39 +0200

    build: ajout SASS

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 
 echo "Starting build process..."
 mkdir -p dist/web
-rm dist/web/*
+rm -r dist/web/*
 cp src/resources/content/* dist/web/
 cp dist/data.js dist/web/data.js
 cp node_modules/cytoscape/dist/cytoscape.min.js dist/web/
+npx sass src/resources/content/style/style.scss dist/web/style.css
 echo "Build completed successfully."
